3d344f1c5b33c5fa7ca63bc481506b3b0ecafe17
Aleksei Bulash <winchesrers@gmail.com>
2026-02-23 00:05:54 +0300

    feat: import videos, update classes and layout, create page config obj

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,23 +1,53 @@
-import catImage from '../../assets/images/cat.png';
+import './404.scss';
+
+import video1 from '../../assets/video/error-video_1.mp4';
+import video2 from '../../assets/video/error-video_2.mp4';
+import video3 from '../../assets/video/error-video_3.mp4';
+import PageTitle from '../../components/page-title/PageTitle';
+import { useState } from 'react';
 
 const notFoundPageClasses = {
   page: 'not-found',
+  textContainer: 'not-found__text-container',
   title: 'not-found__title',
   subtitle: 'not-found__subtitle',
+  video: 'not-found__video',
+  buttonsContainer: 'not-found__buttons-container',
+  button: 'not-found__button',
 };
 
-const notFoundTexts = {
+const notFoundPageTexts = {
   title: '404',
-  subtitle: 'okak',
+  subtitle: 'Ooops! Page not found :(',
   alt: '404 image',
+  backButton: 'Go Back',
+  homePageButton: 'Go Homepage',
 };
+
+const notFoundPageConfig = {
+  title: 'Page not found | asyncmind',
+  description: 'Page not found',
+};
+
+const videos = [video1, video2, video3];
 
 export default function NotFoundPage() {
+  const [video] = useState(() => videos[Math.floor(Math.random() * videos.length)]);
+
   return (
-    <div className={notFoundPageClasses.page}>
-      <h1 className={notFoundPageClasses.title}>{notFoundTexts.title}</h1>
-      <img src={catImage} alt={notFoundTexts.alt} />
-      <h2 className={notFoundPageClasses.subtitle}>{notFoundTexts.subtitle}</h2>
-    </div>
+    <>
+      <PageTitle pageName={notFoundPageConfig.title} description={notFoundPageConfig.description} />
+      <div className={notFoundPageClasses.page}>
+        <video src={video} className={notFoundPageClasses.video} autoPlay loop muted />
+        <div className={notFoundPageClasses.textContainer}>
+          <h1 className={notFoundPageClasses.title}>{notFoundPageTexts.title}</h1>
+          <h2 className={notFoundPageClasses.subtitle}>{notFoundPageTexts.subtitle}</h2>
+        </div>
+        <div className={notFoundPageClasses.buttonsContainer}>
+          <button className={notFoundPageClasses.button}>{notFoundPageTexts.backButton}</button>
+          <button className={notFoundPageClasses.button}>{notFoundPageTexts.homePageButton}</button>
+        </div>
+      </div>
+    </>
   );
 }
