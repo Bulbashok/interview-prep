3aca337de53810c3679fec127055aa56cb2b761d
Aleksei Bulash <winchesrers@gmail.com>
2026-02-24 01:02:23 +0300

    feat: import not found page component and add in route path

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { appRoutes, authRoutes, protectedRoutes } from './routes';
+import NotFoundPage from '../pages/404/404';
 
 const HomePage = () => <div> Home Page </div>;
 const Login = () => <div> Login Page </div>;
@@ -7,7 +8,6 @@ const Register = () => <div> Register Page </div>;
 const Profile = () => <div> Profile Page </div>;
 const Dashboard = () => <div> Dashboard Page </div>;
 const Library = () => <div> Library Page </div>;
-const NotFound = () => <div> 404 Page </div>;
 
 export default function AppRouter() {
   return (
@@ -19,7 +19,7 @@ export default function AppRouter() {
         <Route path={protectedRoutes.profile} element={<Profile />} />
         <Route path={protectedRoutes.dashboard} element={<Dashboard />} />
         <Route path={protectedRoutes.library} element={<Library />} />
-        <Route path={appRoutes.notFound} element={<NotFound />} />
+        <Route path={appRoutes.notFound} element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
