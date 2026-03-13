20726250ee03cc3b514da2d2443c2aa8ef91d034
Aleksei Bulash <winchesrers@gmail.com>
2026-03-13 14:43:49 +0300

    feat: add user data interface for dashboard

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -4,3 +4,15 @@ export interface HistoryRecordData {
   allQuestions: number;
   date: string;
 }
+
+export interface DashboardUserData {
+  name: string;
+  progress: number;
+  xp: number;
+  maxExp: number;
+  streak: {
+    current: number;
+    best: number;
+  };
+  history: HistoryRecordData[];
+}
