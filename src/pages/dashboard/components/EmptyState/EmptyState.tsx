f2c1fccc6ca693d041857a74122b0f06f10e550f
Aleksei Bulash <winchesrers@gmail.com>
2026-03-13 15:48:31 +0300

    feat: connect i18n with empty state component

--- src/pages/dashboard/components/EmptyState/EmptyState.tsx
+++ src/pages/dashboard/components/EmptyState/EmptyState.tsx
@@ -1,10 +1,18 @@
+import { i18nKeys } from '@/i18n/i18n-keys';
 import './EmptyState.scss';
 
+import { useTranslation } from 'react-i18next';
+
 export default function DashboardEmptyState({ name }: { name: string }) {
+  const { t } = useTranslation();
+
   return (
     <div className="empty-state">
-      <h1 className="empty-state__title">{name}, добро пожаловать!</h1>
-      <p className="empty-state__message">Перейдите в библиотеку и начните тренировку, успехов!</p>
+      <h1 className="empty-state__title">
+        {name}
+        {t(i18nKeys.dashboard.empty.title)}
+      </h1>
+      <p className="empty-state__message">{t(i18nKeys.dashboard.empty.message)}</p>
     </div>
   );
 }
