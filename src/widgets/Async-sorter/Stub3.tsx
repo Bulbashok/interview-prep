eea4796ab3bfefd25fe71bf9ae13e8e3d357190b
Aleksei Bulash <winchesrers@gmail.com>
2026-03-28 21:26:23 +0300

    feat: update Widget render component

--- src/widgets/Async-sorter/Stub3.tsx
+++
@@ -1,5 +0,0 @@
-import { WidgetProps } from '@/types/widgets';
-// eslint-disable-next-line
-export default function WidgetStub3(_props: WidgetProps) {
-  return <div> Заглушка 1 </div>;
-}
--- src/widgets/WidgetRender.tsx
+++ src/widgets/WidgetRender.tsx
@@ -1,34 +1,19 @@
-import { WidgetsType, WidgetData } from '../types/widgets';
-import WidgetStub1 from './Quiz/Stub1';
-import WidgetStub2 from './TrueFalse/Stub2';
-import WidgetStub3 from './Async-sorter/Stub3';
+import { Widget } from '@/types/widgets';
+import { WidgetStrategyRegistry } from './strategies/widgetStrategyRegistry';
 
-const widgetNames = {
-  widget1: 'quiz',
-  widget2: 'true/false',
-  widget3: 'async-sorter',
-} as const;
+const widgetStrategyRegistry = new WidgetStrategyRegistry();
 
 interface WindgetRenderProps {
-  widget: WidgetsType;
-  data: WidgetData;
-  sendAnswer: () => void;
+  widget: Widget;
+  sendAnswer: (answer: unknown) => void;
 }
 
 export default function WidgetRender(props: WindgetRenderProps) {
-  const { widget, data, sendAnswer } = props;
+  const { widget, sendAnswer } = props;
 
   const renderWidget = () => {
-    switch (widget) {
-      case widgetNames.widget1:
-        return <WidgetStub1 data={data} sendAnswer={sendAnswer} />;
-
-      case widgetNames.widget2:
-        return <WidgetStub2 data={data} sendAnswer={sendAnswer} />;
-
-      case widgetNames.widget3:
-        return <WidgetStub3 data={data} sendAnswer={sendAnswer} />;
-    }
+    const strategy = widgetStrategyRegistry.getStrategy(widget.type);
+    return strategy.render(widget, sendAnswer);
   };
 
   return renderWidget();
