e45aee1682e37d18e7885cb74da6c4dcdac4cf6d
Aleksei Bulash <winchesrers@gmail.com>
2026-04-01 22:18:50 +0300

    feat: create async sorter types

---
+++ src/types/asyncSorter.ts
@@ -0,0 +1,23 @@
+import { BaseWidget } from './widgets';
+
+export interface AsyncSorterPayload extends BaseWidget {
+  codeSnippet: string;
+  blocks: AsyncSorterBlock[];
+}
+
+export interface AsyncSorterBlock {
+  id: string;
+  code: string;
+  label: string;
+}
+
+export interface AsyncSorterAnswerData {
+  callstack: string[];
+  microtasks: string[];
+  macrotasks: string[];
+  outputOrder: string[];
+}
+
+export interface AsyncSorterAnswer extends AsyncSorterAnswerData {
+  id: string;
+}
