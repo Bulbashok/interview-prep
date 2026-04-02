3b56c46fba4475e4035fe3097468faef78df4626
Aleksei Bulash <winchesrers@gmail.com>
2026-04-02 19:22:47 +0300

    feat: add tests to streak component

---
+++ src/pages/dashboard/components/Streak/Streak.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import Streak from './Streak';
+import { i18nKeys } from '@/i18n/i18n-keys';
+
+const streaks = {
+  current: 5,
+  best: 10,
+};
+
+const renderStreak = () => {
+  return render(<Streak currentStreak={streaks.current} bestStreak={streaks.best} />);
+};
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+  }),
+}));
+
+describe('Dashboard Streak', () => {
+  test('render', () => {
+    renderStreak();
+  });
+
+  test('correct texts in component', () => {
+    renderStreak();
+
+    expect(
+      screen.getByText((content) => content.includes(i18nKeys.dashboard.streak.current)),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText((content) => content.includes(i18nKeys.dashboard.streak.best)),
+    ).toBeInTheDocument();
+  });
+
+  test('check correct streaks values', () => {
+    renderStreak();
+
+    expect(
+      screen.getByText((content) => content.includes(String(streaks.current))),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText((content) => content.includes(String(streaks.current))),
+    ).toBeInTheDocument();
+  });
+});
