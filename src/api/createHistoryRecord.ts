8bd89abf6ca5f4f3d360837a9b685f829d124ebc
Aleksei Bulash <winchesrers@gmail.com>
2026-04-08 18:24:46 +0300

    feat: add try catch

--- src/api/createHistoryRecord.ts
+++ src/api/createHistoryRecord.ts
@@ -3,44 +3,49 @@ import { firestoreService } from '@/services/firestore';
 import { HistoryRecordProps } from '@/types/dashboard';
 import { UserData } from '@/types/firebase';
 import { arrayRemove, arrayUnion } from 'firebase/firestore';
+import { getUserData } from './getUserData';
 
 export const createHistoryRecord = async (
   topicName: string,
   currentProgress: number,
   allQuestions: number,
 ): Promise<void> => {
-  const userId = auth.currentUser?.uid;
+  try {
+    const userId = auth.currentUser?.uid;
 
-  if (!userId) return;
+    if (!userId) return;
 
-  const date = new Date();
-  const formattedDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`;
+    const date = new Date();
+    const formattedDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`;
 
-  const userDoc: UserData | null = await firestoreService.getDocument('users', userId);
+    const userDoc: UserData | null = await getUserData();
 
-  if (!userDoc) return;
+    if (!userDoc) return;
 
-  const record: HistoryRecordProps = {
-    subject: topicName,
-    doneQuestions: currentProgress,
-    allQuestions: allQuestions,
-    date: formattedDate,
-  };
+    const record: HistoryRecordProps = {
+      subject: topicName,
+      doneQuestions: currentProgress,
+      allQuestions: allQuestions,
+      date: formattedDate,
+    };
 
-  const existingRecord = userDoc.history.find(
-    (record) =>
-      record.subject === topicName &&
-      record.date === formattedDate &&
-      record.doneQuestions === currentProgress,
-  );
+    const existingRecord = userDoc.history.find(
+      (record) =>
+        record.subject === topicName &&
+        record.date === formattedDate &&
+        record.doneQuestions === currentProgress,
+    );
 
-  if (existingRecord) {
+    if (existingRecord) {
+      await firestoreService.updateDocument('users', userId, {
+        history: arrayRemove(existingRecord),
+      });
+    }
+
     await firestoreService.updateDocument('users', userId, {
-      history: arrayRemove(existingRecord),
+      history: arrayUnion(record),
     });
+  } catch (error) {
+    console.error('Failded to create history record:', error);
   }
-
-  await firestoreService.updateDocument('users', userId, {
-    history: arrayUnion(record),
-  });
 };
