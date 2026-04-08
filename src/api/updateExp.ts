b818384afcd4d1514c1243a7fb86832e5e13a495
Aleksei Bulash <winchesrers@gmail.com>
2026-04-08 18:25:18 +0300

    feat: add try catch

--- src/api/updateExp.ts
+++ src/api/updateExp.ts
@@ -3,11 +3,15 @@ import { firestoreService } from '@/services/firestore';
 import { increment } from 'firebase/firestore';
 
 export const updateExp = async (): Promise<void> => {
-  const userId = auth.currentUser?.uid;
+  try {
+    const userId = auth.currentUser?.uid;
 
-  if (!userId) return;
+    if (!userId) return;
 
-  await firestoreService.updateDocument('users', userId, {
-    currentExp: increment(10),
-  });
+    await firestoreService.updateDocument('users', userId, {
+      currentExp: increment(10),
+    });
+  } catch (error) {
+    console.error('Failed to update user experience', error);
+  }
 };
